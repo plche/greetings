cdb72a4ac84b19e297018dc79309f0fefb3edc33
plche <che.percy.l@gmail.com>
2022-02-14 12:25:03 -0500

    Final commit

---
+++ Test.java
@@ -0,0 +1,12 @@
+// File name must be the same as the class name
+// Every file IS a class
+public class Test {
+
+    // The file or class with the exact method signature below
+    // is always the launching point of the application
+    // You cannot run a Java program without it.
+	public static void main(String[] args) {
+        // For now, all your code goes here!
+        System.out.println("Hello, world!");
+	}
+}
